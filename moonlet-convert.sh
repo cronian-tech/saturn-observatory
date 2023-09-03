5e4f70c9b09014b54e5839aa2e4d845d78d34527
Elisey Zanko <elisey.zanko@gmail.com>
2023-09-03 16:39:48 +0500

    Use gzip for snapshot and csv

--- moonlet-convert.sh
+++ moonlet-convert.sh
@@ -12,8 +12,8 @@ VM_EXPORT_URL=http://localhost:8428/api/v1/export/csv
 docker run --name victoria-metrics -p 127.0.0.1:8428:8428 -v /snapshot -d victoriametrics/victoria-metrics:${VM_VERSION}
 
 # Un-tar Prometheus snapshot into VictoriaMetrics volume.
-docker run --rm --volumes-from victoria-metrics -w /snapshot -v "$(pwd)/snapshot.tar:/snapshot.tar" busybox \
-    tar -xvf /snapshot.tar --strip 1
+docker run --rm --volumes-from victoria-metrics -w /snapshot -v "$(pwd)/snapshot.tar.gz:/snapshot.tar.gz" busybox \
+    tar -xzvf /snapshot.tar.gz --strip 1
 
 # Import the snapshot into VictoriaMetrics.
 docker run --rm -it --volumes-from victoria-metrics --network container:victoria-metrics victoriametrics/vmctl:${VM_VERSION} \
@@ -22,12 +22,13 @@ docker run --rm -it --volumes-from victoria-metrics --network container:victoria
 
 # Export metrics from VictoriaMetrics container to CSV on the host.
 METRIC_NAME=saturn_node_info
-METRIC_FORMAT=__timestamp__,id,state,core,ip_address,sunrise,cassini,geoloc_region,geoloc_city,geoloc_country,geoloc_country_code,sppedtest_isp,sppedtest_server_location,sppedtest_server_country
+METRIC_FORMAT=__timestamp__:rfc3339,id,state,core,ip_address,sunrise,cassini,geoloc_region,geoloc_city,geoloc_country,geoloc_country_code,sppedtest_isp,sppedtest_server_location,sppedtest_server_country
 curl --create-dirs ${VM_EXPORT_URL} \
     -o "data/${METRIC_NAME}.csv" \
     -d "format=${METRIC_FORMAT}" \
     -d "match[]=${METRIC_NAME}"
 sed -i '' -e "1s/^/${METRIC_FORMAT}\n/" "data/${METRIC_NAME}.csv"
+gzip "data/${METRIC_NAME}.csv"
 
 # Remove VictoriaMetrics container and its volume.
 docker rm -vf victoria-metrics
--- moonlet-snapshot.sh
+++ moonlet-snapshot.sh
@@ -7,7 +7,7 @@ snapshot=$(curl -XPOST http://localhost:9090/api/v1/admin/tsdb/snapshot | jq -er
 
 # Copy the snapshot from Promeheus container volume to the host.
 docker run --rm --volumes-from saturn-moonlet-prometheus-1 -v "$(pwd):/snapshot" busybox \
-   tar -cvf /snapshot/snapshot.tar -C /prometheus/snapshots "${snapshot}"
+   tar -czvf /snapshot/snapshot.tar.gz -C /prometheus/snapshots "${snapshot}"
 
 # Remove snapshot from Prometheus container volume.
 docker exec saturn-moonlet-prometheus-1 rm -r "/prometheus/snapshots/${snapshot}"
