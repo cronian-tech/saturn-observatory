2965fd0be6cc60ed715f7e566bde233b73a51a51
Elisey Zanko <elisey.zanko@gmail.com>
2023-09-09 12:54:06 +0500

    Remove Python plotting

--- plot.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/outputs/saturn_active_nodes.csv", header=None, parse_dates=[0])
-print(df)
-
-res = df.plot(x=0, y=1).get_figure()
-res.savefig("data/saturn_active_nodes.png")
