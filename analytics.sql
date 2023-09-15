84bb4081f38dbc3581ee859f3ffce5926d3902c4
Elisey Zanko <elisey.zanko@gmail.com>
2023-09-15 17:32:33 +0500

    Refactor analytics SQL and plotting

--- analytics.sql
+++ analytics.sql
@@ -14,30 +14,6 @@ COPY (
     ORDER BY observed_at -- Ordering is required for deterministic results.
 ) TO '/outputs/saturn_active_node.csv';
 
--- For every active node returns its age (in days).
-COPY (
-    SELECT
-        node_id,
-        datepart('day', observed_at - epoch_ms(created_at)) AS age_days
-    FROM (
-        SELECT
-            node_id,
-            max(observed_at) AS observed_at
-        FROM saturn_node_info
-        WHERE state = 'active'
-        GROUP BY node_id
-    )
-    JOIN (
-        SELECT
-            node_id,
-            any_value(created_at) AS created_at
-        FROM saturn_node_creation
-        GROUP BY node_id
-    )
-    USING (node_id)
-    ORDER BY node_id -- Ordering is required for deterministic results.
-) TO '/outputs/saturn_active_node_age.csv';
-
 -- Returns the number of active nodes by country.
 COPY (
     SELECT
@@ -49,45 +25,68 @@ COPY (
     ORDER BY geoloc_country -- Ordering is required for deterministic results.
 ) TO '/outputs/saturn_active_node_by_country.csv';
 
--- For every active node returns its approximate estimated earnings.
+-- For every active node return various calculated stats.
 COPY (
-    SELECT
-        node_id,
-        coalesce(estimated_earnings_fil, 0) as estimated_earnings_fil,
-    FROM (
+    WITH
+    -- When a node was last observed active.
+    last_active AS (
+        SELECT
+            node_id,
+            max(observed_at) AS last_active_at
+        FROM saturn_node_info
+        WHERE state = 'active'
+        GROUP BY node_id
+    ),
+    -- When a node was created.
+    created AS (
+        SELECT
+            node_id,
+            any_value(created_at) AS created_at
+        FROM saturn_node_creation
+        GROUP BY node_id
+    ),
+    -- For every active node returns its last observed age (in days).
+    node_age AS (
+        SELECT
+            node_id,
+            datepart('day', last_active_at - epoch_ms(created_at)) AS age_days
+        FROM last_active
+        JOIN created
+        USING (node_id)
+    ),
+    -- List all active nodes.
+    active_node AS (
         SELECT DISTINCT node_id
         FROM saturn_node_info
         WHERE state = 'active'
-    )
-    LEFT OUTER JOIN (
+    ),
+    -- Calculate estimated earnings per node.
+    node_estimated_earnings AS (
         SELECT
             node_id,
             sum(estimated_earnings_fil) as estimated_earnings_fil
         FROM saturn_node_estimated_earnings
-        WHERE observed_at >= '2023-08-02' -- Earnings on 2023-08-01 looks too high. Need to figure out this later.
+        -- Earnings on 2023-08-01 looks abnormally high. Need to figure out this later.
+        WHERE observed_at >= '2023-08-02'
         GROUP BY node_id
-    )
-    USING (node_id)
-    ORDER BY node_id
-) TO '/outputs/saturn_active_node_estimated_earnings.csv';
-
--- For every active node returns how much traffic it served.
-COPY (
-    SELECT
-        node_id,
-        coalesce(bandwidth_served_bytes, 0) as bandwidth_served_bytes,
-    FROM (
-        SELECT DISTINCT node_id
-        FROM saturn_node_info
-        WHERE state = 'active'
-    )
-    LEFT OUTER JOIN (
+    ),
+    -- Calculate traffic served per node.
+    node_bandwidth_served AS (
         SELECT
             node_id,
             sum(bandwidth_served_bytes) as bandwidth_served_bytes
         FROM saturn_node_bandwidth_served
         GROUP BY node_id
     )
-    USING (node_id)
-    ORDER BY node_id
-) TO '/outputs/saturn_active_node_bandwidth_served.csv';
+    -- Final join.
+    SELECT
+        node_id,
+        age_days,
+        coalesce(estimated_earnings_fil, 0) as estimated_earnings_fil,
+        coalesce(bandwidth_served_bytes, 0) as bandwidth_served_bytes,
+    FROM active_node
+    JOIN node_age USING (node_id)
+    LEFT OUTER JOIN node_estimated_earnings USING (node_id)
+    LEFT OUTER JOIN node_bandwidth_served USING (node_id)
+    ORDER BY node_id -- Ordering is required for deterministic results.
+) TO '/outputs/saturn_active_node_stats.csv';
